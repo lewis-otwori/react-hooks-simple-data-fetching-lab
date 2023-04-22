dc767f815bf335eaec633d4086662005f0d8f7be
lewis-otwori <lewisotwori@gmail.com>
2023-04-22 13:58:05 +0300

    displays a loading message before fetching

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, {useEffect, useState} from 'react'
+
+function App() {
+const [smallDog, setsmallDog] =useState([])
+const [imageUrl, setImageUrl] =useState(null);
+
+useEffect(() => {
+fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then(data => {
+            setsmallDog(data.message);
+            setImageUrl(true)
+        });
+
+})
+ if(!imageUrl) return <p>Loading...</p>;
+
+  return (
+   <div>
+      <img src={smallDog} alt= "A Random Dog" /> 
+    </div>
+  )
+}
+
+export default App
